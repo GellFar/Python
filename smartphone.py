caa211f891d58127972ff44e41d2ea6ecec0b736
Gellfarson <seekingofthevoid@gmail.com>
2024-12-03 14:59:08 +0300

    lesson3

---
+++ smartphone.py
@@ -0,0 +1,5 @@
+class Smartphone:
+    def __init__(self, mark_t, model_t, num_t):
+        self.mark_t = mark_t
+        self.model_t = model_t
+        self.num_t = num_t
---
+++ uslovie.py
@@ -0,0 +1,12 @@
+import random
+chislo = random.randint(1, 10)
+print("Аттракцион невиданной щедрости." +
+      "Сейчас выпадет рандомной число и скажет что тебе сделать!")
+if (chislo <= 5):
+    print(chislo, "= Сходил бы ты на хуй")
+
+elif (chislo > 6):
+    print(chislo, "= Ты идёшь на хуй")
+
+else:
+    print(chislo, "Съебал нахуй")
